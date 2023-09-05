abe30b8a78e1bf20548ebabe057545c4147c20ff
Uche <uuchei2@illinois.edu>
2023-09-04 20:51:10 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Homepage from "./components/Homepage";
+import OptionSelect from "./components/OptionSelectPage"
+import MediaSelect from "./components/MediaSelectPage"
+import ScreenshotMedia from './components/ScreenshotMediaPage';
+import EndOperation from './components/EndOperationPage';
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
+import React, { useState } from 'react';
+
+// Theme for App
+const theme = createTheme({
+  palette: {
+    primary: 
+      {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+      },
+    secondary: 
+      {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+      }
+  },
+});
+    
 function App() {
+
+  const [pageMedia, setPageMedia] = useState([]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>
+        <Routes>
+          <Route exact path="/" element={<Homepage setPageMedia = {setPageMedia} />} />
+          <Route exact path="/optionselectpage" element={<OptionSelect />} />
+          <Route exact path="/mediaselectpage" element={<MediaSelect />} />
+          <Route exact path="/screenshotpage" element={<ScreenshotMedia />} />
+          <Route exact path="/endoperationpage" element={<EndOperation />} />
+        </Routes>
+      </MemoryRouter>
+    </ThemeProvider>
+    
   );
 }
 
